5b5a2561a7f53041932d0629dd8a0d6ebadbc892
Jeoongu <ms235280@naver.com>
2024-06-28 23:44:20 +0900

    [feat] : kopring init

---
+++ src/main/kotlin/com/aledma/kopringdemo/KopringDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.aledma.kopringdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/aledma/kopringdemo/KopringDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aledma.kopringdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
